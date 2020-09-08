7a849ab01701fb8b0c6781aeb5cbbbcbc723b8b4
Rminix <rmiiniix@gmail.com>
2020-09-08 13:24:13 +0430

    Fix typo in models.quotes

--- app/models/quotes.py
+++ app/models/quotes.py
@@ -4,8 +4,8 @@ from sqlalchemy import orm, func
 class QuotesModel(db.Model):
     __tablename__ = 'quotes'
     _id = db.Column(db.Integer, primary_key=True)
-    quote =  db.Column(db.string(255), nullable=False )
-    author = db.Column(db.string(60), nullable=False) 
+    quote =  db.Column(db.String(255), nullable=False )
+    author = db.Column(db.String(60), nullable=False) 
 
     normalize_quote = orm.column_property(func.lower(quote))
     normalize_author = orm.column_property(func.lower(author))
