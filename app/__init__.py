3ecead8e384232949a6fa18b4a6c5b1f85b37d4f
Rminix <rmiiniix@gmail.com>
2020-09-08 12:59:32 +0430

    Initialize sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from app.config import DevelopmentConfig, ProductionConfig
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
 
+    db.init_app(app)
+
+
     from app.resources.homepage import Home
     api.add_resource(Home, '/')
 
+
+    with app.app_context():
+        db.create_all()
     return app
