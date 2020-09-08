c817d69301907c03f888daf4b75295e5a86d5985
Rminix <rmiiniix@gmail.com>
2020-09-08 13:13:11 +0430

    Register quote resource

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,21 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     api = Api(app)
+    if app.config["ENV"] == 'production':
+        app.config.from_object(ProductionConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)
+
 
     db.init_app(app)
 
 
     from app.resources.homepage import Home
     api.add_resource(Home, '/')
+    from app.resources.quotes import Quotes
+    api.add_resource(Quotes, '/api/quote/create')
+    from app.resources.quotes import RandomQuote
+    api.add_resource(RandomQuote, '/api/quote/random')
 
 
     with app.app_context():
